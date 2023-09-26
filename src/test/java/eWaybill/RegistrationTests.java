8ce0953f15ad0245651578ed44d0ab7eae4d21f2
NafithQA <emoqbel@nafith.com>
2023-09-27 01:08:28 +0300

    Editing Registration Test

--- src/test/java/eWaybill/RegistrationTests.java
+++ src/test/java/eWaybill/RegistrationTests.java
@@ -4,10 +4,9 @@ import eWaybill.base.Configurations;
 import eWaybill.helpers.Functions;
 import eWaybill.pageModels.LoginPage;
 import eWaybill.pageModels.SignUpPage;
-import org.openqa.selenium.By;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 public class RegistrationTests extends Configurations {
@@ -27,8 +26,35 @@ public class RegistrationTests extends Configurations {
         signUpPage.fillInCompanyRegistrationNumberField(randomCompanyRegistrationNumber);
         signUpPage.clickOnBusinessActivityTypeDropDown();
         signUpPage.clickOnFreightForwarderOption();
-        signUpPage.clickOnCompanyRegistrationCertificateBrowseFileButton();
+        functions.uploadAttachment(signUpPage.getFirstAttachmentButton(), System.getProperty("user.dir")
+                + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "img1.jpg");
+        functions.scrollThePageDown();
+        signUpPage.clickOnNextButton();
+        functions.scrollThePageUp();
+        signUpPage.clickOnCountryDropDown();
+        signUpPage.clickOnGovernorateDropDown();
+        signUpPage.clickOnMuscatOption();
+        signUpPage.fillInAddressField("Muscat");
+        String randomPhoneNumber = String.valueOf(functions.generateRandomNumber(10));
+        signUpPage.fillInCompanyTelephoneNoField(randomPhoneNumber);
+        signUpPage.clickOnNextButton();
+        signUpPage.fillInFullNameField("Al Shefaa User");
+        signUpPage.clickOnUserNationalityDropDown();
+        signUpPage.clickOnOmanOption();
+        signUpPage.clickOnIdDocumentTypeDropDown();
+        signUpPage.clickOnPassportOption();
+        String randomIdDocumentNumber = String.valueOf(functions.generateRandomNumber(12));
+        signUpPage.fillInIdDocumentNoField(randomIdDocumentNumber);
+        String randomAuthorizedManagerPhoneNumber = String.valueOf(functions.generateRandomNumber(10));
+        signUpPage.fillInAuthorizedManagerMobileNoField(randomAuthorizedManagerPhoneNumber);
+        signUpPage.fillInEmailField(functions.getRandomEmailString() + "@gmail.com");
+        functions.scrollThePageDown();
+        functions.uploadAttachment(signUpPage.getFirstAttachmentButton(), System.getProperty("user.dir")
+                + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "img1.jpg");
+        functions.uploadAttachment(signUpPage.getSecondAttachmentButton(), System.getProperty("user.dir")
+                + File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "img2.jpg");
+        signUpPage.clickOnNextButton();
 
-        Thread.sleep(4000);
+        Thread.sleep(5000);
     }
 }
